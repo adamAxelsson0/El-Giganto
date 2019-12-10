83d2f8e92541e3045e6f10077c2698256a63e45e
Adam Axelsson <adam.axelsson1992@gmail.com>
2019-12-10 15:06:47 +0100

    Initial Setup

---
+++ ClassLibrary/Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace ClassLibrary
+{
+    public class Database
+    {
+        private readonly string connectionString;
+        public Database(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
