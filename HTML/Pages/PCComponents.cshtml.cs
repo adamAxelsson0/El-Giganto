f4436b98cb5c5ec95538ce8bb5ea226b8f52475b
Adam Axelsson <adam.axelsson1992@gmail.com>
2020-01-08 18:12:33 +0100

    fixed navbar

--- HTML/Pages/PCComponents.cshtml.cs
+++ HTML/Pages/PCComponents.cshtml.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ClassLibrary;
 
 namespace HTML.Pages
 {
-    public class HOmeModel : PageModel
+    public class PCComponentsModel : PageModel
     {
         public void OnGet()
         {
